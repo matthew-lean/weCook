1a97c6d9ef8cf15307f7c65bc6eeb20af4e65c89
matthew-lean <matt.1001@live.co.uk>
2017-05-09 11:12:03 +0100

    jQuery add ingredients now counts, changing the id and name to be correct for input to have an individual value

--- webroot/js/scripts.js
+++ webroot/js/scripts.js
@@ -18,10 +18,21 @@ $(document).ready(function(){
   //   count++;
   // });
 
+  var count = 1;
 
   $('#IngredientAdd').click(function() {
-    // Add new step input field dynamically
-    $('#newStep').append('<input type="text" name="steps[1][description]" placeholder="Ingredient" maxlength="144" id="steps-1-description">');
+    // Add new step input field
+    $('#newStep').append('<input type="text" name="steps['+ count++ +'][description]" placeholder="Ingredient" maxlength="144" id="steps-'+ (count-1) +'-description">');
+    // console log the amount of input boxes
     console.log($('#newStep').children().length);
   });
+
+  // $('#IngredientAdd').click(function() {
+  //   var lastname = $('#newStep [name^=value]:last-child')[0].name;
+  //   var next = parseInt(lastname.match(/\d+/)[0]) + 1;
+  //   $('#newStep').append('<input type="text" name="steps[' + next + ']" value="more more text">');
+  //   // console log the amount of input boxes
+  //   console.log($('#newStep').children().length);
+  // });
+
 });
